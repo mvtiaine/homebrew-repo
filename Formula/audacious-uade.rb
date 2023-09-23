ddeb39a344b43d9485a1417485409080ec0c28c0
Matti Tiainen <mvtiaine@cc.hut.fi>
2023-09-23 19:30:25 +0300

    audacious-uade 0.8.2

--- Formula/audacious-uade.rb
+++ Formula/audacious-uade.rb
@@ -1,11 +1,13 @@
 class AudaciousUade < Formula
   desc "UADE plugin for Audacious media player"
   homepage "https://github.com/mvtiaine/audacious-uade"
-  url "https://github.com/mvtiaine/audacious-uade", using: :git, tag: "0.8.1", revision: "7b8308edcee707613709facb940b39642596a147"
   license "GPL-2.0-or-later"
 
-  depends_on "autoconf" => :build
-  depends_on "autoconf-archive" => :build
+  stable do
+    url "https://github.com/mvtiaine/audacious-uade/releases/download/0.8.2/audacious-uade-0.8.2.tar.bz2"
+    sha256 "914e77ac4eebc4bbd35074a97e40831a20927ec9f714eee3ad204437d39a6daf"
+  end
+
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
@@ -16,7 +18,6 @@ class AudaciousUade < Formula
   end
 
   def install
-    system "autoreconf", "-i"
     system "./configure", "--with-audacious-plugindir=\"#{lib}/audacious\"", *std_configure_args
     system "make"
     system "make", "install"
