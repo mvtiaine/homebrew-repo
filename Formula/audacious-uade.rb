8056bcf99acd84c453ab0b78550f52088ddc089b
Matti Tiainen <mvtiaine@cc.hut.fi>
2023-11-03 12:15:45 +0200

    audacious-uade 0.9.1

--- Formula/audacious-uade.rb
+++ Formula/audacious-uade.rb
@@ -1,7 +1,7 @@
 class AudaciousUade < Formula
   desc "UADE plugin for Audacious media player"
   homepage "https://github.com/mvtiaine/audacious-uade"
-  url "https://github.com/mvtiaine/audacious-uade/releases/download/0.9/audacious-uade-0.9.tar.bz2"
+  url "https://github.com/mvtiaine/audacious-uade/releases/download/0.9.1/audacious-uade-0.9.1.tar.bz2"
   sha256 "06ec2db917c3c6968fb540838b4c7ec61bb2fae41dd43f878511772dbfd5e6e9"
   license "GPL-2.0-or-later"
 
