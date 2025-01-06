70461cb51bbff2666ef2950b26892b0b265715fb
Matti Tiainen <mvtiaine@cc.hut.fi>
2025-01-06 13:02:04 +0200

    audacious-uade 0.12.4

--- Formula/audacious-uade.rb
+++ Formula/audacious-uade.rb
@@ -1,15 +1,15 @@
 class AudaciousUade < Formula
   desc "UADE plugin for Audacious media player"
   homepage "https://github.com/mvtiaine/audacious-uade"
-  url "https://github.com/mvtiaine/audacious-uade/releases/download/0.12.3/audacious-uade-0.12.3.tar.bz2"
-  sha256 "9d49a041551696511b61db0b1c31e0fc379633be5696de26b63876d94e31a932"
+  url "https://github.com/mvtiaine/audacious-uade/releases/download/0.12.4/audacious-uade-0.12.4.tar.bz2"
+  sha256 "3d1b5f5dc42e04f9cc8c355b4783644507dcc36e13ac7e2662cad3b84432ff91"
   license "GPL-2.0-or-later"
 
   depends_on "pkgconf" => :build
   depends_on "audacious"
 
   def install
-    system "./configure", "--with-audacious-plugindir=\"#{lib}/audacious\"", *std_configure_args
+    system "./configure", "--enable-plugin-audacious=yes", "--with-static-stdlibs=no", "--with-audacious-plugindir=\"#{lib}/audacious\"", *std_configure_args
     system "make"
     system "make", "install"
   end
@@ -19,9 +19,9 @@ class AudaciousUade < Formula
       IMPORTANT!
       In order for Homebrew Audacious to find the plugin, you must manually symlink it to the plugin dir:
 
-      on macOS:
+      macOS:
           ln -s "${HOMEBREW_PREFIX}/lib/audacious/Input/uade.dylib" "$(pkgconf --variable=plugin_dir audacious)/Input/uade.dylib"
-      on Linux:
+      Linux:
           ln -s "${HOMEBREW_PREFIX}/lib/audacious/Input/uade.so" "$(pkgconf --variable=plugin_dir audacious)/Input/uade.so"
     EOS
   end
